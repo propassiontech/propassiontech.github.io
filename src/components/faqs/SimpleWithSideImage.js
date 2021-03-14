06a4d922cd5912394df2671c72f887c398c6cde0
jillek <jille@designindulgence.com>
2021-03-14 14:42:31 +0800

    Create a React app and publish it to GitHub Pages

---
+++ src/components/faqs/SimpleWithSideImage.js
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
+import { ReactComponent as PlusIcon } from "feather-icons/dist/icons/plus.svg";
+import { ReactComponent as MinusIcon } from "feather-icons/dist/icons/minus.svg";
+
+const Container = tw.div`relative`;
+const Content = tw.div`max-w-screen-xl mx-auto py-16 lg:py-20`;
+
+const TwoColumn = tw.div`flex`;
+const Column = tw.div``;
+
+const Image = styled.div(props => [
+  `background-image: url("${props.imageSrc}");`,
+  props.imageContain ? tw`bg-contain bg-no-repeat` : tw`bg-cover`,
+  props.imageShadow ? tw`shadow` : tw`shadow-none`,
+  tw`hidden lg:block rounded h-144 bg-center`
+]);
+
+const FAQContent = tw.div`lg:ml-12`;
+const Subheading = tw(SubheadingBase)`mb-4 text-center lg:text-left`;
+const Heading = tw(SectionHeading)`lg:text-left`;
+const Description = tw.p`max-w-xl text-center mx-auto lg:mx-0 lg:text-left lg:max-w-none leading-relaxed text-sm sm:text-base lg:text-lg font-medium mt-4 text-secondary-100`;
+
+const FAQSContainer = tw.dl`mt-12`;
+const FAQ = tw.div`cursor-pointer mt-8 select-none border lg:border-0 px-8 py-4 lg:p-0 rounded-lg lg:rounded-none`;
+const Question = tw.dt`flex justify-between items-center`;
+const QuestionText = tw.span`text-lg lg:text-xl font-semibold`;
+const QuestionToggleIcon = styled.span`
+  ${tw`ml-2 bg-primary-500 text-gray-100 p-1 rounded-full group-hover:bg-primary-700 group-hover:text-gray-200 transition duration-300`}
+  svg {
+    ${tw`w-4 h-4`}
+  }
+`;
+const Answer = motion.custom(tw.dd`pointer-events-none text-sm sm:text-base leading-relaxed`);
+
+export default ({
+  subheading = "",
+  heading = "Questions",
+  description = "Here are some frequently asked questions about our calibration from our professional customers. Should you have any other questions, feel free to reach out via the contact form below.",
+  imageSrc = "https://images.unsplash.com/photo-1529854140025-25995121f16f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1500&q=80",
+  imageContain = false,
+  imageShadow = true,
+  faqs = null
+}) => {
+  /*
+   * You can modify FAQs either by modifying the below defaultFaqs array or by passing a custom array of FAQs using
+   * the faqs prop
+   */
+  const defaultFaqs = [
+    {
+      question: "Is the firmware provided free of cost ?",
+      answer:
+        "Yes, it is. The chips contain firmware."
+    },
+    {
+      question: "What is your MOQ?",
+      answer:
+        "500pcs. And sample orders are welcome"
+    },
+    {
+      question: "Where to get a sample?",
+      answer:
+        "Email us or get in touch via wechat."
+    },
+    {
+      question: "Where can I reach you for support ?",
+      answer:
+        "Wechat."
+    }
+  ];
+
+  if (!faqs || faqs.length === 0) faqs = defaultFaqs;
+
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(null);
+
+  const toggleQuestion = questionIndex => {
+    if (activeQuestionIndex === questionIndex) setActiveQuestionIndex(null);
+    else setActiveQuestionIndex(questionIndex);
+  };
+
+  return (
+    <Container>
+      <Content>
+        <TwoColumn>
+          <Column tw="hidden lg:block w-5/12 flex-shrink-0">
+            <Image imageContain={imageContain} imageShadow={imageShadow} imageSrc={imageSrc} />
+          </Column>
+          <Column>
+            <FAQContent>
+              {subheading ? <Subheading>{subheading}</Subheading> : null}
+              <Heading>{heading}</Heading>
+              <Description>{description}</Description>
+              <FAQSContainer>
+                {faqs.map((faq, index) => (
+                  <FAQ
+                    key={index}
+                    onClick={() => {
+                      toggleQuestion(index);
+                    }}
+                    className="group"
+                  >
+                    <Question>
+                      <QuestionText>{faq.question}</QuestionText>
+                      <QuestionToggleIcon>
+                        {activeQuestionIndex === index ? <MinusIcon /> : <PlusIcon />}
+                      </QuestionToggleIcon>
+                    </Question>
+                    <Answer
+                      variants={{
+                        open: { opacity: 1, height: "auto", marginTop: "16px" },
+                        collapsed: { opacity: 0, height: 0, marginTop: "0px" }
+                      }}
+                      initial="collapsed"
+                      animate={activeQuestionIndex === index ? "open" : "collapsed"}
+                      transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
+                    >
+                      {faq.answer}
+                    </Answer>
+                  </FAQ>
+                ))}
+              </FAQSContainer>
+            </FAQContent>
+          </Column>
+        </TwoColumn>
+      </Content>
+    </Container>
+  );
+};
---
+++ src/components/features/TwoColSingleFeatureWithStats.js
@@ -0,0 +1,75 @@
+import React from "react";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import { SectionHeading } from "components/misc/Headings.js";
+
+const Container = tw.div`relative`;
+const TwoColumn = tw.div`flex flex-col md:flex-row justify-between max-w-screen-xl mx-auto py-20 md:py-24`;
+const Column = tw.div`w-full max-w-md mx-auto md:max-w-none md:mx-0`;
+const ImageColumn = tw(Column)`md:w-6/12 lg:w-5/12 flex-shrink-0 h-80 md:h-auto`;
+const TextColumn = styled(Column)(props => [
+  tw`md:w-6/12 mt-8 md:mt-0`,
+  props.textOnLeft ? tw`md:mr-8 lg:mr-16 md:order-first` : tw`md:ml-8 lg:ml-16 md:order-last`
+]);
+
+const Image = styled.div(props => [
+  `background-image: url("${props.imageSrc}");`,
+  tw`rounded bg-cover bg-center h-full`,
+]);
+const TextContent = tw.div`lg:py-8`;
+
+const Heading = tw(SectionHeading)`text-left text-3xl sm:text-4xl lg:text-5xl text-center md:text-left leading-tight`;
+const Description = tw.p`text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100 mt-4`
+
+const Statistics = tw.div`mt-6 lg:mt-8 xl:mt-16 flex flex-wrap`
+const Statistic = tw.div`text-lg sm:text-2xl lg:text-3xl w-1/2 mt-4 lg:mt-10 text-center md:text-left`
+const Value = tw.div`font-bold text-primary-500`
+const Key = tw.div`font-medium text-gray-700`
+
+export default ({textOnLeft = false}) => {
+  // The textOnLeft boolean prop can be used to display either the text on left or right side of the image.
+  //Change the statistics variable as you like, add or delete objects
+  const statistics = [
+    {
+      key: "Channels",
+      value: "512",
+    },
+    {
+      key: "Colors",
+      value: "16 million",
+    },
+    {
+      key: "ms speed",
+      value: "0.2093",
+    },
+    {
+      key: "Calibrations",
+      value: "10347",
+    }
+  ]
+
+  return (
+    <Container>
+      <TwoColumn>
+        <ImageColumn>
+          <Image imageSrc="https://images.unsplash.com/photo-1612521564381-8b0d19cc4951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80" />
+        </ImageColumn>
+        <TextColumn textOnLeft={textOnLeft}>
+          <TextContent>
+            <Heading>The best technology is ready for you.</Heading>
+            <Description>Get the highest quality of light with zero effort.</Description>
+            <Statistics>
+              {statistics.map((statistic, index) => (
+              <Statistic key={index}>
+                <Value>{statistic.value}</Value>
+                <Key>{statistic.key}</Key>
+              </Statistic>
+              ))}
+            </Statistics>
+          </TextContent>
+        </TextColumn>
+      </TwoColumn>
+    </Container>
+  );
+};
